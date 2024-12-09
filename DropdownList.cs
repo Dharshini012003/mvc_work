25e66932860b0bc5c83200b9832531b6d1fe58e9
D2003 <104339002+Dharshini012003@users.noreply.github.com>
2024-12-09 06:46:19 +0530

    Add files via upload

---
+++ DropdownList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeWebApp.Models
+{
+    public class DropdownList
+    {
+        // Get Data Manager for "Gender" or any other type
+        public static IEnumerable<SelectListItem> GetDataManager(string typeId)
+        {
+            using (TRAININGEntities1 db = new TRAININGEntities1())
+            {
+                var lst = db.DATA_MANAGER.Where(w => w.DM_TYPE_ID == typeId).Select(o => new SelectListItem{Value = o.DM_VALUE_ID,Text = o.DM_VALUE_DESC}).ToList();
+                lst.Insert(0, new SelectListItem { Value = "", Text = "" }); 
+                return lst;
+            }
+        }
+
+        // Get Department List
+        public static IEnumerable<SelectListItem> GetDepartment()
+        {
+            using (TRAININGEntities1 db = new TRAININGEntities1())
+            {
+                var lst = db.DEPTMNTs.AsEnumerable().Select(o => new SelectListItem{Value = o.DEPT_ID.ToString(), Text = o.DEPT_DESC}).ToList();
+                lst.Insert(0, new SelectListItem { Value = "", Text = "" }); 
+                return lst;
+            }
+        }
+
+        // Get Designation List
+        public static IEnumerable<SelectListItem> GetDesignation(int? selectedDesignationId = null)
+        {
+            using (TRAININGEntities1 db = new TRAININGEntities1())
+            {
+                var lst = db.DESIGINATIONs.AsEnumerable().Select(o => new SelectListItem{Value = o.DES_ID.ToString(),Text = o.DES_DESC, Selected = (o.DES_ID == selectedDesignationId) }).ToList();
+                lst.Insert(0, new SelectListItem { Value = "", Text = "" }); 
+                return lst;
+            }
+        }
+    }
+}
---
+++ RepoEmployee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EmployeWebApp.Models;
+
+namespace EmployeWebApp.Repository
+{
+    public class RepoEmployee
+    {
+        TRAININGEntities1 db = new TRAININGEntities1();
+
+        public List<EMPLOYEE> GetList()
+        {
+            List<EMPLOYEE> empList = db.EMPLOYEEs.ToList();
+            return empList;
+        }
+
+        public EMPLOYEE GetEmpDetails(int id)
+        {
+            EMPLOYEE employee = new EMPLOYEE();
+            if (id == 0)
+            {
+                employee = new EMPLOYEE();
+            }
+            else
+            {
+                employee = db.EMPLOYEEs.SingleOrDefault(e => e.EMP_ID == id);
+            }
+            return employee;
+        }
+    }
+}
